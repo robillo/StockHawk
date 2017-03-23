786162ed645c9d01d2a05539539e26247c78347f
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-23 20:22:36 +0530

    VH

---
+++ app/src/main/java/com/appbusters/robinkamboj/stockhawk/controller/View_Holder.java
@@ -0,0 +1,20 @@
+package com.appbusters.robinkamboj.stockhawk.controller;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.appbusters.robinkamboj.stockhawk.R;
+
+public class View_Holder extends RecyclerView.ViewHolder{
+
+    private TextView stock_symbol, stock_name, seeds, percent;
+
+    public View_Holder(View itemView) {
+        super(itemView);
+        stock_symbol = (TextView) itemView.findViewById(R.id.stock_symbol);
+        stock_name = (TextView) itemView.findViewById(R.id.stock_name);
+        seeds = (TextView) itemView.findViewById(R.id.seeds);
+        percent = (TextView) itemView.findViewById(R.id.percent);
+    }
+}
