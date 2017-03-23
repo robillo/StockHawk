d17c719babec98ec21a5655124528fb2794e8d77
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-23 20:53:04 +0530

    Dummy Data to be filled now.

--- app/src/main/java/com/appbusters/robinkamboj/stockhawk/model/Data.java
+++ app/src/main/java/com/appbusters/robinkamboj/stockhawk/model/Data.java
@@ -4,6 +4,13 @@ public class Data {
 
     private String stock_symbol, stock_name, seeds, percent;
 
+    public Data(String stock_symbol, String stock_name, String seeds, String percent) {
+        this.stock_symbol = stock_symbol;
+        this.stock_name = stock_name;
+        this.seeds = seeds;
+        this.percent = percent;
+    }
+
     public String getStock_symbol() {
         return stock_symbol;
     }
--- app/src/main/java/com/appbusters/robinkamboj/stockhawk/view/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/stockhawk/view/MainActivity.java
@@ -4,14 +4,25 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.appbusters.robinkamboj.stockhawk.R;
+import com.appbusters.robinkamboj.stockhawk.controller.RVAdapter;
+import com.appbusters.robinkamboj.stockhawk.model.Data;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RVAdapter adapter;
+    private LinearLayoutManager layoutManager;
+    List<Data> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        data = fillWithData();
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        adapter = new RVAdapter(data, R.layout.row_layout, getApplicationContext());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private List<Data> fillWithData(){
+        data.add(new Data("YHOO", "Yahoo! Inc.", "0.143", "+46%"));
+        return data;
     }
 
     @Override
