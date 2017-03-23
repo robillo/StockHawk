4ed230a85d960b53b4d39072fad9fa1cbf600020
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-23 20:17:55 +0530

    Temp Data

---
+++ app/src/main/java/com/appbusters/robinkamboj/stockhawk/model/Data.java
@@ -0,0 +1,38 @@
+package com.appbusters.robinkamboj.stockhawk.model;
+
+public class Data {
+
+    private String stock_symbol, stock_name, seeds, percent;
+
+    public String getStock_symbol() {
+        return stock_symbol;
+    }
+
+    public void setStock_symbol(String stock_symbol) {
+        this.stock_symbol = stock_symbol;
+    }
+
+    public String getStock_name() {
+        return stock_name;
+    }
+
+    public void setStock_name(String stock_name) {
+        this.stock_name = stock_name;
+    }
+
+    public String getSeeds() {
+        return seeds;
+    }
+
+    public void setSeeds(String seeds) {
+        this.seeds = seeds;
+    }
+
+    public String getPercent() {
+        return percent;
+    }
+
+    public void setPercent(String percent) {
+        this.percent = percent;
+    }
+}
--- app/src/main/java/com/appbusters/robinkamboj/stockhawk/view/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/stockhawk/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.appbusters.robinkamboj.stockhawk;
+package com.appbusters.robinkamboj.stockhawk.view;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.appbusters.robinkamboj.stockhawk.R;
 
 public class MainActivity extends AppCompatActivity {
 
